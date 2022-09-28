afc72d81cb3701705ad5c45899ae0f7fa3afa00f
daria-iliashevskaia <>
2022-09-28 12:02:49 +0300

    fix universal_file_hh method, add vacancies file rewriting for vacancies from HH.ru

--- Vacancy_finder/classes.py
+++ Vacancy_finder/classes.py
@@ -25,8 +25,8 @@ class HH(Engine):
             url = 'https://api.hh.ru/vacancies'
             par = {"text": "python", 'area': '113', 'per_page': '10', 'page': i}
             response = requests.get(url, params=par)
-            response_json = response.json()
-            job_list.append(response_json)
+            response_json = response.json()["items"]
+            job_list.extend(response_json)
         return job_list
 
 
@@ -36,13 +36,21 @@ class Superjob(Engine):
         """
         Возвращает список с данными о вакансии с HH
         """
-        # считывает данные только со второй страницы, нужно дописать цикл
+        key_word = "python"
         num_page = 2
-        url = f"https://russia.superjob.ru/vacancy/search/?keywords=python&page={num_page}"
+        url = f"https://russia.superjob.ru/vacancy/search/?keywords={key_word}&page={num_page}"
         response = requests.get(url)
         soup = BS(response.text, "lxml")
-        job_list_superjob = soup.findAll("div", class_="f-test-search-result-item")
-        return job_list_superjob
+        # job_list_superjob = soup.findAll("div", class_="f-test-search-result-item")
+        # print(job_list_superjob[0])
+
+        name = soup.find_all("span", class_="_9fIP1 _249GZ _1jb_5 QLdOc")
+        description = soup.find_all("span", class_="_1Nj4W _249GZ _1jb_5 _1dIgi _3qTky")
+        salary = soup.find_all("span", class_="_2eYAG _1nqY_ _249GZ _1jb_5 _1dIgi")
+
+        # print(name)
+        # print(description)
+        # print(salary)
 
 
 class Vacancy:
@@ -53,25 +61,38 @@ class Vacancy:
         self.salary = None
         self.description = None
 
-    def __str__(self) -> str:
-        return json.dumps(self.__dict__, ensure_ascii=False, indent=4)
+    def __repr__(self) -> str:
+        return f"Название вакансии: {self.title}\n" \
+               f"Ссылка на вакансию: {self.url}\n" \
+               f"Зарплата: {self.salary}\n" \
+               f"Описание: {self.description}\n" \
+               f"_________________________________\n"
 
     def parse_vacancy_hh(self):
         """
-        Записывает значения полей объекта класса Vacancy в соответствии с найденной вакансией
-        Нужно: менять переменные полей согласно списку, создавать объект на каждую вакансию, записывать этот объект в файл с помощью repr
+        Записывает значения полей объекта класса Vacancy в соответствии с найденной вакансией,
+        меняет переменные полей согласно списку,
+        создаёт объект на каждую вакансию,
+        записывает этот объект в файл с помощью __repr__
         """
         hh_vacances = HH()
         job_list = hh_vacances.get_request()
-        universal_file_list = universal_file_hh(job_list) # пофиксить метод universal_file_hh (выдаёт список из одинаковых значений)
+        universal_file_list = universal_file_hh(job_list)
 
+        file_vacances_list = []
+        # создаю список со строковыми значениями объектов вакансий
         for job in universal_file_list:
             self.title = job["name"]
             self.url = job["url"]
             self.salary = job["salary"]
             self.description = job["description"]
+            file_vacances_list.append(str(v))
 
-            print(v)
+            with open("vacances.txt", "w", encoding="utf-8") as file:
+                # записываю значения объектов вакансий с hh в файл
+                for vacancy in file_vacances_list:
+                    file.write(vacancy)
+
 
 
 
--- Vacancy_finder/utils.py
+++ Vacancy_finder/utils.py
@@ -6,14 +6,12 @@ def universal_file_hh(file_to_convert) -> list:
     """
     universal_file = {}
     universal_file_list = []
-
     for el in file_to_convert:
-        universal_file["name"] = el["items"][0]["name"]
-        universal_file["salary"] = el["items"][0]["salary"]
-        universal_file["url"] = el["items"][0]["alternate_url"]
-        universal_file["description"] = el["items"][0]["snippet"]
-
-        universal_file_list.append(universal_file)
+        universal_file["name"] = el["name"]
+        universal_file["salary"] = el["salary"]
+        universal_file["url"] = el["alternate_url"]
+        universal_file["description"] = el["snippet"]
+        universal_file_list.append(universal_file.copy())
 
     return universal_file_list
 
