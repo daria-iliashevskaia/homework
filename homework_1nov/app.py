b02751d878ade679949818cf0900f836f774c8ea
daria-iliashevskaia <>
2022-11-01 14:40:21 +0300

    add homework from 1 of november

---
+++ homework_1nov/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+# обработчик для просмотра страницы пользователя с использованием шаблона
+@app.route('/users/<int:id>')
+def users(id):
+    return render_template(
+        'users/show.html',
+        id=id
+    )
+
+# обработчик для вывода списка пользователей с возможностью фильтрации
+@app.route('/users/')
+def all_users():
+    users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
+    term = request.args.get('term')
+    filtered_users = filter(lambda x: term in x, users)
+    return render_template(
+        'users/index.html',
+        users=filtered_users,
+        search=term
+    )
+
+
+app.run(debug=True)
