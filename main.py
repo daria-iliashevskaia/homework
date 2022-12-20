6282977a860272552f10f3c87c7e084afada44c6
daria-iliashevskaia <>
2022-12-20 18:09:50 +0300

    cash function realized

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def select_sorted(sort_columns, filename, limit=10):
     :param filename:
     :param limit:
     """
+
     df = pd.read_csv('all_stocks_5yr.csv')
-    sorted_df = df.sort_values(by=sort_columns, ascending=False)
-    res = sorted_df.head(limit)
-    res.to_csv(filename)
+    cash = pd.read_csv("sorted.csv")
+    if (cash['id']. eq(str(sort_columns))).any():
+        print(cash.loc[cash['id'] == str(sort_columns)].head(limit))
+    else:
+        sorted_df = df.sort_values(by=sort_columns, ascending=False)
+        res = sorted_df.head(limit)
+        res.insert(0, "id", str(sort_columns))
+        res.to_csv(filename, mode='a')
 
 
-# select_sorted(sort_columns=["high"], limit=10, filename='sorted.csv')
+select_sorted(sort_columns=["low"], limit=10, filename='sorted_cash.csv')
 
 
 def get_data(date, name, filename):
@@ -29,14 +35,15 @@ def get_data(date, name, filename):
         reader = csv.reader(f)
         next(reader)
 
-        with open(filename, 'w') as f1:
+        with open(filename, 'w', newline='') as f1:
             writer = csv.writer(f1)
-            writer.writerow(['date', 'open', 'high', 'low', 'close', 'volume', 'name'])
+            writer.writerow(['id', 'date', 'open', 'high', 'low', 'close', 'volume', 'name'])
 
             for row in reader:
                 if row[0] == date and row[6] == name:
                     writer.writerow(row)
 
 
-# get_data(date = "2013-12-05", name="AIZ", filename="filtered.csv")
+
+# get_data(date="2013-12-05", name="AIZ", filename="filtered.csv")
 
